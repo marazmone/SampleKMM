2bcd3dc54f373aac8bba8a3ba30bf5e57b9a6b8e
Serhii Hulenko <sergei.gulenko@gmail.com>
2022-05-24 13:50:04 +0200

    add ios app with storyboard

---
+++ iosApp/iosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosApp
+//
+//  Created by user on 24.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
